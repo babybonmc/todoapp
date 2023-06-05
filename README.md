70b52329dcf30a97da5ee9c2c2b5be5ee3621416
babybonmc <115027379+babybonmc@users.noreply.github.com>
2023-06-05 14:45:44 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# todoapp
+## tdd
+
+> This app is written only for demonstration purposes.
+
+Solution for the [Exercise 3 - To-do list client application](https://github.com/timotr/harjutused/blob/main/hajusrakendused/todo-client.md) written in Rust. 
+
+## Usage
+
+### Cloning the project and installing the dependencies: 
+
+- [clap](https://crates.io/crates/clap)
+- [reqwest](https://crates.io/crates/reqwest)
+- [tokio](https://crates.io/crates/tokio)
+- [colored](https://crates.io/crates/colored)
+- [serde](https://crates.io/crates/serde)
+- [serde_json](https://crates.io/crates/serde_json)
+
+```
+git clone https://github.com/babybonmc/todoapp
+cd todoapp
+cargo run
+```
+This will launch the program in a development environment. 
+
+In order to build a binary file, command
+```
+cargo build --release
+```
+could be used.
+
+For further information use:
+```
+cargo r -- -h
+```
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use args::Args;
+use clap::Parser;
+use colored::*;
+use logging::{logging, logout};
+use task::{create_task, delete_task, get_tasks, update_task, view_task};
+use user::show_profile;
+
+mod args;
+mod consts;
+mod errors;
+mod logging;
+mod task;
+mod user;
+
+fn main() {
+    logging();
+
+    let args = Args::parse();
+
+    match args {
+        Args {
+            show_profile: true, ..
+        } => show_profile(12).unwrap(),
+        Args {
+            create_task: true, ..
+        } => create_task().unwrap(),
+        Args {
+            get_tasks: true, ..
+        } => get_tasks().unwrap(),
+        Args {
+            delete_task: id, ..
+        } if id > 0 => delete_task(id).expect("Invalid ID"),
+        Args {
+            update_task: id, ..
+        } if id > 0 => update_task(id).expect("Invalid ID"),
+        Args { view_task: id, .. } if id > 0 => view_task(id).expect("Invalid ID"),
+        Args { logout: true, .. } => logout().unwrap(),
+        _ => println!("{}", "-h for information about the commands".italic()),
+    }
+}
